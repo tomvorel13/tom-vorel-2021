d352dc62235d00c078b23b2fd34c8d66387ecfce
Tom Vorel <tomvorel13@gmail.com>
2021-06-28 10:26:34 +0200

    Initial formatting

--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-					<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
